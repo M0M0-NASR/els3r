5d4882518f42273af5ca6a02b29dab3eeb2b86e2
unknown <mohamednasrmahmood@gmail.com>
2024-02-19 19:40:53 +0200

    Update setup of front end

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    welcome to Home Page
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view("/","product.index");
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
