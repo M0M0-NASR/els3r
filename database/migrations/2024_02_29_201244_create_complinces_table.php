6347cda096bcd551e69ac602b301741e1eddd278
unknown <mohamednasrmahmood@gmail.com>
2024-03-02 01:29:03 +0200

    Craete Compliance Table with migration

---
+++ database/migrations/2024_02_29_201244_create_complinces_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('complinces', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->string('full_name');
+            $table->string('ssn', 14);
+            $table->string('shop_name');
+            $table->string('shop_address');
+            $table->string('government');
+            $table->string('content');
+            $table->enum('status', ['معلقة', 'يتم النظر', 'تم الحل']);
+
+            // Composite Unique Key
+            $table->unique(['product_id', 'ssn']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('complinces');
+    }
+};
