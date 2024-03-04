d7c0d7c5d37a0cfc9aea78f72f9135de3c1165a9
unknown <mohamednasrmahmood@gmail.com>
2024-03-05 00:02:11 +0200

    Update website logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{asset('assets/img/Els3r logo.ico')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('assets/img/Els3r logo.ico')}}" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
