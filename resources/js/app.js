5b5df7b84af39e8691e3ceb3006bb24ad59798d8
unknown <mohamednasrmahmood@gmail.com>
2024-03-11 06:32:25 +0200

    Fix bugs: Fix Category.index view

--- resources/js/app.js
+++ resources/js/app.js
@@ -43,6 +43,24 @@ const productsSwiper = new Swiper(".products .mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+      
+      breakpoints: {
+        345: {
+            slidesPerView: 1,
+        },
+        // when window width is <= 576px
+        576: {
+            slidesPerView: 1,
+        },
+        // when window width is <= 768px
+        768: {
+            slidesPerView: 2,
+        },
+        // when window width is <= 992px
+        992: {
+            slidesPerView: 3,
+        },
+    },
   });
 
   
